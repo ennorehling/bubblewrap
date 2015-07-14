3359d92fa5ec7c9fb724d8779b84d8d0fd907164
Enno Rehling <enno.rehling@gmail.com>
2015-07-14 03:09:40 +0200

    build script

--- build.bat
+++ build.bat
@@ -6,15 +6,15 @@ mkdir dist
 REM create game.love
 "c:\Program Files\7-Zip\7z.exe" a dist\%GAME%.love *.lua res\*.*
 
-PAUSE
-
 REM create windows exe
 mkdir build
 copy /b "c:\Program Files\LOVE\love.exe" + dist\%GAME%.love build\%GAME%.exe
 
 REM build windows zip
 "c:\Program Files\7-Zip\7z.exe" a dist\%GAME%.zip build\%GAME%.exe "c:\Program Files\LOVE\*.dll"
-
 rd /s /q build
+
+dir dist\*.*
+copy dist\*.* C:\Users\Enno\Dropbox\LOVE\
 
 PAUSE
